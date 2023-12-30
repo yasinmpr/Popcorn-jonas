f4b0c5b464fdda49a56f9eae24c33310757c7906
Yasin-mpr <Yasinmpr1@gmail.com>
2023-12-30 16:52:39 +0330

    first part of project - add header component and some styles

---
+++ popcorn_project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Header from "./Components/HeaderComponent";
+import "./css/main.css";
+
+function App() {
+    return (
+        <>
+            <div className="main">
+                <Header />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ popcorn_project/src/Components/HeaderComponent.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "../css/Header.css";
+
+function Header() {
+    return (
+        <>
+            <header>
+                <p className="title">🍿usePopcorn</p>
+                <form action="" className="searchMovie">
+                    <input
+                        type="text"
+                        className="searchBoxMovie"
+                        placeholder="search movies ..."
+                    />
+                </form>
+                <p className="resultNumber">Found 0 result</p>
+            </header>
+        </>
+    );
+}
+
+export default Header;
